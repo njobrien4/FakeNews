bde51fc79c9709957e84be06f1769468b3e326f7
Nicole O'Brien <njobrien@mit.edu>
2018-03-18 09:56:37 -0400

    abs val

--- cnn-text-classification-tf (body)/eval.py
+++ cnn-text-classification-tf (body)/eval.py
@@ -212,12 +212,14 @@ best_n_trigrams = interpret.get_best_n_for_each_neuron(best_trigrams,15)
 write_trigram_dict('best_trigrams.txt',best_trigrams)
 write_trigram_dict('best_n_trigrams.txt',best_n_trigrams)
 
-best_neurons_fake, best_neurons_real = interpret.get_n_best_neurons(weights,10)
+best_neurons_fake, best_neurons_real, worst_neurons_fake, worst_neurons_real = interpret.get_n_best_neurons(weights,10)
 #make_weight_histogram(weights)
 best_fake_neurons = {key : best_n_trigrams[key] for key in best_neurons_fake}
 best_real_neurons = {key: best_n_trigrams[key] for key in best_neurons_real}
 write_trigram_dict('best_n_fake_neurons.txt',best_fake_neurons)
+write_trigram_dict('worst_n_fake_neurons.txt', worst_neurons_fake)
 write_trigram_dict('best_n_real_neurons.txt', best_real_neurons)
+write_trigram_dict('worst_n_real_neurons.txt',worst_neurons_real)
 
 np.save("weights",weights)
 
